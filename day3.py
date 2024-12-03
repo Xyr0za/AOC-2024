c8d4fbc4ea7323899c87ae8f69862e983d8faece
Euan Baldwin <102750293+Xyr0za@users.noreply.github.com>
2024-12-03 09:21:00 +0000

    Day 3 Solved

---
+++ day3.py
@@ -0,0 +1,38 @@
+import re
+
+reg = r"mul\(\d{1,3},\d{1,3}\)"
+cont = ''.join([line.strip() for line in open("day3input.txt")])
+
+matches = re.findall(reg, cont)
+
+t = 0
+
+for match in matches:
+    int_conv = lambda x : [int(i) for i in x]
+    m1, m2 = int_conv(re.findall(r"\d{1,3}", match))
+    t += (m1 * m2)
+
+reger = r"don't\(\)|do\(\)|mul\(\d{1,3},\d{1,3}\)"
+matches = re.findall(reger, cont)
+
+s = 0
+is_on = True
+
+for match in matches:
+    print(match, is_on)
+    if match == "don't()":
+        print("off")
+        is_on = False
+        continue
+    if match == "do()":
+        print("on")
+        is_on = True
+        continue
+
+    if is_on:
+        int_conv = lambda x : [int(i) for i in x]
+        m1, m2 = int_conv(re.findall(r"\d{1,3}", match))
+        s += (m1 * m2)
+
+
+print(s)
